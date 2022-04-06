46bbbbec2587e2e18f66c995c9d6b0d833e5e6cc
arjunbeliever <78067269+arjunbeliever@users.noreply.github.com>
2022-04-06 16:35:15 +0530

    Update ethclient.go

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -84,7 +84,6 @@ func (ec *Client) BlockByHash(ctx context.Context, hash common.Hash) (*types.Blo
 // Note that loading full blocks requires two requests. Use HeaderByNumber
 // if you don't need all transactions or uncle headers.
 func (ec *Client) BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error) {
-	fmt.Printf("BlockByNumber called", number)
 	return ec.getBlock(ctx, "eth_getBlockByNumber", toBlockNumArg(number), true)
 }
 
